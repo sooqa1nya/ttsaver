eba37c749b90112b8a942fa01b12e4c720d1ced9
sooqa1nya <sooqa1nya@gmail.com>
2026-07-21 15:12:36 +0700

    rotate download methods

--- src/tools/get-methods.ts
+++ src/tools/get-methods.ts
@@ -6,13 +6,13 @@ import { ytdl } from '../services/yt-dl';
 export const getMethods = (link: string) => {
     const isTikTok = /tiktok/.test(link);
     return [
-        ...(isTikTok ? [
-            () => ttApiDl.getFilesV3(link),
-            () => tikwm.getFiles(link),
-            () => ttApiDl.getFilesV2(link),
-            () => ttApiDl.getFilesV1(link)
-        ] : []),
         () => ytdl.download(link),
         () => cobalt.getFiles(link),
+        ...(isTikTok ? [
+            () => ttApiDl.getFilesV2(link),
+            () => tikwm.getFiles(link),
+            () => ttApiDl.getFilesV3(link),
+            () => ttApiDl.getFilesV1(link),
+        ] : []),
     ];
 };
